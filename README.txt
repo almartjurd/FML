f252b639761d97a2b338680871d366150a096790
almartjurd <tjeerdcorjanus@gmail.com>
2026-05-08 11:39:01 +0200

    v2.2
    - Stats updates
    - Sounds updates
    - Dices updates

--- README.txt
+++ README.txt
@@ -1,3 +1,2 @@
-Fellow Wizard Lords PWA
-Upload all files to your GitHub Pages repo root.
-Visit your GitHub Pages URL to install.
+Fellow Magic Lords PWA
+Upload all files to GitHub Pages root.
--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
 const CACHE='fwl-v3';
 self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
